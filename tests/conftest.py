4389b4076c5446eed19389b63b24da7669c52352
Artsiom_Sinitski <Artsiom.Sinitski@burberry.com>
2022-06-21 14:35:49 +0300

    Simplified project to avoid configuring webhooks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,28 @@
 import pytest
-import pyodbc
+# import pyodbc
 
 
 @pytest.fixture(scope='module')
 def db_session(variables):
-    server = variables.get("server")
-    db_name = variables.get("db_name")
-    db_user = variables.get("db_user")
-    db_pwd = variables.get("db_pwd")
-    db_session = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};"
-                                "SERVER=" + server + ";DATABASE=" + db_name +
-                                ";UID=" + db_user + ";PWD=" + db_pwd)
-    print(f"\n*** Connected to '{db_name}' database! ***")
-    yield db_session
-    print(f"\n*** Disconnected from '{db_name}' database! ***")
-    db_session.close()
+    pass
+    # server = variables.get("server")
+    # db_name = variables.get("db_name")
+    # db_user = variables.get("db_user")
+    # db_pwd = variables.get("db_pwd")
+    # db_session = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};"
+    #                             "SERVER=" + server + ";DATABASE=" + db_name +
+    #                             ";UID=" + db_user + ";PWD=" + db_pwd)
+    # print(f"\n*** Connected to '{db_name}' database! ***")
+    # yield db_session
+    # print(f"\n*** Disconnected from '{db_name}' database! ***")
+    # db_session.close()
 
 
 @pytest.fixture
 def db_cursor(db_session):
-    cursor = db_session.cursor()
-    yield cursor
+    pass
+    # cursor = db_session.cursor()
+    # yield cursor
 
 
 @pytest.fixture
